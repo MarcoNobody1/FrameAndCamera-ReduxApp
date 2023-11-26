ffa562146b052615dfd22ea71ea0df0113dc4fcd
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-26 04:40:46 +0100

    se crea usestate para compartir el input del searchinput con la mediacard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { HomeAppBar } from "../components/HomeAppBar";
 
 import { MediaCard } from "../components/MediaCard";
 
 export const Home = () => {
+  const [searchInput, setSearchInput] = useState();
+  const setSearch = (value) => {
+    setSearchInput(value);
+  };
+
   return (
     <>
-      <HomeAppBar />
-      <MediaCard />
+      <HomeAppBar setSearch={setSearch}/>
+      <MediaCard search={searchInput}/>
     </>
   );
 };
