59a3d3f9b69b13c943ec270ce7f390cb52d623be
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-26 04:41:40 +0100

    se corrigen errores en el nuevo thunk

--- src/features/search/searchThunks.js
+++ src/features/search/searchThunks.js
@@ -4,13 +4,14 @@ const baseUrl = "https://api.unsplash.com/";
 const random30 = "photos/random?count=30";
 const random1 = "photos/random?count=1"
 const search = "search/photos?query=";
-const search1 = "&per_page=1";
 
-const clientId = "client_id=cpDUshpWYRdkrCVucZevE9z5iRVI_Bq01VV8Z7HRDlE";
-//const clientId2 = "client_id=y7WCJuIpaCe_Ajeix9l-J0g9aTj1Lha5YuEJWbp3Ldo";
+const search1 = "photos/random?query=";
+
+//const clientId = "client_id=cpDUshpWYRdkrCVucZevE9z5iRVI_Bq01VV8Z7HRDlE";
+const clientId2 = "client_id=y7WCJuIpaCe_Ajeix9l-J0g9aTj1Lha5YuEJWbp3Ldo";
 //const clientId3 = "client_id=nXYdqrC3SY0K5xz_xLvHlgzJQrHnoCpNXQr8uX-cx10";
 export const getPhotos = createAsyncThunk("search/getPhotos", async () => {
-  const request = await fetch(`${baseUrl}${random30}&${clientId}`);
+  const request = await fetch(`${baseUrl}${random30}&${clientId2}`);
   const rawphotos = await request.json();
 
   return rawphotos.map((rawphoto) => {
@@ -31,7 +32,7 @@ export const getPhotos = createAsyncThunk("search/getPhotos", async () => {
 });
 
 export const get1Photo = createAsyncThunk("search/get1Photo", async () => {
-  const request = await fetch(`${baseUrl}${random1}&${clientId}`);
+  const request = await fetch(`${baseUrl}${random1}&${clientId2}`);
   const rawphotos = await request.json();
   const onephoto = {
     id: rawphotos[0].id,
@@ -51,7 +52,7 @@ export const get1Photo = createAsyncThunk("search/get1Photo", async () => {
 });
 
 export const searchPhotos = createAsyncThunk("search/searchPhotos", async (searching) => {
-  const request = await fetch(`${baseUrl}${search}${searching}&${clientId}`);
+  const request = await fetch(`${baseUrl}${search}${searching}&${clientId2}`);
   const searchedphotos = await request.json();
   return searchedphotos.results.map((searchphoto) => {
     return {
@@ -71,21 +72,20 @@ export const searchPhotos = createAsyncThunk("search/searchPhotos", async (searc
 })
 
 export const search1Photo = createAsyncThunk("search/search1Photo", async (searching) => {
-  const request = await fetch(`${baseUrl}${search}${searching}${search1}&${clientId}`);
+  const request = await fetch(`${baseUrl}${search1}${searching}&${clientId2}`);
   const searchedphoto = await request.json();
-  return searchedphoto.results.map((searchphoto) => {
-    return {
-      id: searchphoto[0].id,
-      date: searchphoto[0].created_at,
-      width: searchphoto[0].width,
-      height: searchphoto[0].height,
-      altDesc: searchphoto[0].alt_description,
-      url: searchphoto[0].urls.regular,
-      download: searchphoto[0].urls.full,
-      likes: searchphoto[0].likes,
-      city: searchphoto[0].user.first_name,
-      country: searchphoto[0].user.username,
-      append: 'by ',
-    };
-  });
+  const onephoto = {
+    id: searchedphoto.id,
+    date: searchedphoto.created_at,
+    width: searchedphoto.width,
+    height: searchedphoto.height,
+    altDesc: searchedphoto.alt_description,
+    url: searchedphoto.urls.regular,
+    download: searchedphoto.urls.full,
+    likes: searchedphoto.likes,
+    city: searchedphoto.location?.city,
+    country: searchedphoto.location?.country,
+    append: 'by '
+  }
+  return onephoto;
 })
