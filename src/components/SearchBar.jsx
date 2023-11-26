899c2baf8f10165c0ec860d750f38c6693f11af8
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-26 04:59:24 +0100

    se recogen props para setear la variable del inputsearch

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,47 +5,50 @@ import { searchPhotos } from "../features/search/searchThunks";
 import { clearPhotos } from "../features/search/searchSlice";
 import { useLocation } from "react-router-dom";
 import { resetFavorites, searchFavs } from "../features/favorite/favoriteSlice";
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
 const CssTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: '#A0AAB4',
+  "& label.Mui-focused": {
+    color: "#A0AAB4",
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: '#B2BAC2',
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
   },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'transparent',
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "transparent",
     },
-    '&:hover fieldset': {
-      borderColor: 'transparent',
+    "&:hover fieldset": {
+      borderColor: "transparent",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'transparent',
+    "&.Mui-focused fieldset": {
+      borderColor: "transparent",
     },
   },
 });
 
-export const SearchBar = () => {
+export const SearchBar = ({ setSearch }) => {
   const dispatch = useDispatch();
   const location = useLocation();
-  const [searchText, setSearchText] = useState('');
-  const [prevSearchText, setPrevSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [prevSearchText, setPrevSearchText] = useState("");
 
   const onSearchHandler = (event) => {
-  const searchInput = event.target.value;
-
-    searchInput.length > 2 ? dispatch(searchPhotos(searchInput)) : dispatch(clearPhotos());
-  }
+    const searchInput = event.target.value;
+    if (searchInput.length > 2) {
+      dispatch(searchPhotos(searchInput));
+      setSearch(searchInput);
+    } else {
+      dispatch(clearPhotos());
+    }
+  };
 
   useEffect(() => {
     if (searchText.length > prevSearchText.length) {
       dispatch(searchFavs(searchText));
     } else {
       dispatch(resetFavorites());
-      dispatch(searchFavs(searchText)
-      );
+      dispatch(searchFavs(searchText));
     }
     setPrevSearchText(searchText);
   }, [searchText, dispatch, prevSearchText]);
@@ -57,18 +60,22 @@ export const SearchBar = () => {
   return (
     <>
       <CssTextField
-      id="custom-css-outlined-input"
-      fullWidth
-        onChange={location.pathname === "/" ? onSearchHandler : handleInputChange}
-        sx={
-          {
-            borderRadius: "3.125rem",
-            background: "#747474"
-          }
+        id="custom-css-outlined-input"
+        fullWidth
+        onChange={
+          location.pathname === "/" ? onSearchHandler : handleInputChange
         }
-        label={location.pathname === "/" ? "Type here for automatic search!" : "Search photo by description!" }
+        sx={{
+          borderRadius: "3.125rem",
+          background: "#747474",
+        }}
+        label={
+          location.pathname === "/"
+            ? "Type here for automatic search!"
+            : "Search photo by description!"
+        }
         InputLabelProps={{
-          style: { fontWeight: '300', color: 'rgba(255, 255, 255, 0.87)'  }
+          style: { fontWeight: "300", color: "rgba(255, 255, 255, 0.87)" },
         }}
       />
     </>
