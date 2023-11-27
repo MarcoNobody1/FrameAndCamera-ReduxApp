2809b594fb28ebe59c0eace15b840ba2fabc7a7e
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-27 21:27:33 +0100

    Se modifica logica para evitar problemas de fetch

--- src/features/search/searchThunks.js
+++ src/features/search/searchThunks.js
@@ -11,112 +11,69 @@ const keys = [
   process.env.REACT_APP_API_KEY2,
   process.env.REACT_APP_API_KEY3,
   process.env.REACT_APP_API_KEY4,
+  process.env.REACT_APP_API_KEY5,
+  process.env.REACT_APP_API_KEY6,
+  process.env.REACT_APP_API_KEY7,
+  process.env.REACT_APP_API_KEY8,
+  process.env.REACT_APP_API_KEY9,
+  process.env.REACT_APP_API_KEY10,
 ];
 
-let currentKeyIndex = 0;
-let remainingCalls = 50;
-
-const rotateKey = () => {
-  remainingCalls = 50;
-  currentKeyIndex = (currentKeyIndex + 1) % keys.length;
+const fetchDataWithRetry = async (url) => {
+  let request;
+  for (const key of keys) {
+    try {
+      request = await fetch(`${baseUrl}${url}&client_id=${key}`);
+      if (request.status === 200) {
+        break; // Si es exitoso, salir del bucle
+      }
+    } catch (error) {
+      console.error("Error al obtener datos:", error);
+    }
+  }
+  return request;
 };
 
+const processPhoto = (rawphoto) => ({
+  id: rawphoto.id,
+  date: rawphoto.created_at,
+  width: rawphoto.width,
+  height: rawphoto.height,
+  altDesc: rawphoto.alt_description,
+  url: rawphoto.urls.regular,
+  download: rawphoto.urls.full,
+  likes: rawphoto.likes,
+  city: rawphoto.location?.city,
+  country: rawphoto.location?.country,
+  append: "en ",
+});
+
 export const getPhotos = createAsyncThunk("search/getPhotos", async () => {
-  if (remainingCalls === 0) {
-    rotateKey();
-  }
-
-  const request = await fetch(`${baseUrl}${random30}&client_id=${keys[currentKeyIndex]}`);
+  const request = await fetchDataWithRetry(random30);
   const rawphotos = await request.json();
-  remainingCalls--;
-
-  return rawphotos.map((rawphoto) => ({
-    id: rawphoto.id,
-    date: rawphoto.created_at,
-    width: rawphoto.width,
-    height: rawphoto.height,
-    altDesc: rawphoto.alt_description,
-    url: rawphoto.urls.regular,
-    download: rawphoto.urls.full,
-    likes: rawphoto.likes,
-    city: rawphoto.location.city,
-    country: rawphoto.location.country,
-    append: 'in ',
-  }));
+  return rawphotos.map(processPhoto);
 });
 
 export const get1Photo = createAsyncThunk("search/get1Photo", async () => {
-  if (remainingCalls === 0) {
-    rotateKey();
-  }
-
-  const request = await fetch(`${baseUrl}${random1}&${keys[currentKeyIndex]}`);
+  const request = await fetchDataWithRetry(random1);
   const rawphotos = await request.json();
-  remainingCalls--;
-
-  const onephoto = {
-    id: rawphotos[0].id,
-    date: rawphotos[0].created_at,
-    width: rawphotos[0].width,
-    height: rawphotos[0].height,
-    altDesc: rawphotos[0].alt_description,
-    url: rawphotos[0].urls.regular,
-    download: rawphotos[0].urls.full,
-    likes: rawphotos[0].likes,
-    city: rawphotos[0].location.city,
-    country: rawphotos[0].location.country,
-    append: 'in ',
-  };
-
-  return onephoto;
+  return processPhoto(rawphotos[0]);
 });
 
-export const searchPhotos = createAsyncThunk("search/searchPhotos", async (searching) => {
-  if (remainingCalls === 0) {
-    rotateKey();
+export const searchPhotos = createAsyncThunk(
+  "search/searchPhotos",
+  async (searching) => {
+    const request = await fetchDataWithRetry(`${search}${searching}`);
+    const searchedphotos = await request.json();
+    return searchedphotos.results.map(processPhoto);
   }
+);
 
-  const request = await fetch(`${baseUrl}${search}${searching}&${keys[currentKeyIndex]}`);
-  const searchedphotos = await request.json();
-  remainingCalls--;
-
-  return searchedphotos.results.map((searchphoto) => ({
-    id: searchphoto.id,
-    date: searchphoto.created_at,
-    width: searchphoto.width,
-    height: searchphoto.height,
-    altDesc: searchphoto.alt_description,
-    url: searchphoto.urls.regular,
-    download: searchphoto.urls.full,
-    likes: searchphoto.likes,
-    city: searchphoto.user.first_name,
-    country: searchphoto.user.username,
-    append: 'by ',
-  }));
-});
-
-export const search1Photo = createAsyncThunk("search/search1Photo", async (searching) => {
-  if (remainingCalls === 0) {
-    rotateKey();
+export const search1Photo = createAsyncThunk(
+  "search/search1Photo",
+  async (searching) => {
+    const request = await fetchDataWithRetry(`${search1}${searching}`);
+    const searchedphoto = await request.json();
+    return processPhoto(searchedphoto);
   }
-
-  const request = await fetch(`${baseUrl}${search1}${searching}&${keys[currentKeyIndex]}`);
-  const searchedphoto = await request.json();
-  remainingCalls--;
-
-  const onephoto = {
-    id: searchedphoto.id,
-    date: searchedphoto.created_at,
-    width: searchedphoto.width,
-    height: searchedphoto.height,
-    altDesc: searchedphoto.alt_description,
-    url: searchedphoto.urls.regular,
-    download: searchedphoto.urls.full,
-    likes: searchedphoto.likes,
-    city: searchedphoto.location?.city,
-    country: searchedphoto.location?.country,
-    append: 'by ',
-  };
-
-  return onephoto;
-});
+);
